af94c7b47c519817201cdb4fcfba62271cce3ee8
user2 <user2@znyr.io>
2020-12-12 20:05:44 +0530

    fixed

--- src/server.js
+++ src/server.js
@@ -17,31 +17,64 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.post("/", async (req, res) => {
   console.log(req.body.name);
 
-
   try {
     fs.mkdir(`${req.body.id}`, (err, folder) => {
-      if (err) console.log(err);
-      else {
-        console.log("The temporary folder path is:", folder);
+      if (err) {
+        console.log(err);
+      } else {
         if (req.body.name == "python") {
           fs.writeFile(`${req.body.id}/run.py`, req.body.code, function (err) {
             if (err) throw err;
             console.log("Saved!");
           });
-          PythonShell.run(`${req.body.id}/run.py`, null, function (err, results) {
-            if (err) {
-              res.send(err.message);
-              console.log(err.messag);
+          PythonShell.run(
+            `${req.body.id}/run.py`,
+            null,
+            function (err, results) {
+              if (err) {
+                console.log(req.body.id);
+                fs.rmdir(
+                  `${req.body.id}`,
+                  {
+                    recursive: true,
+                  },
+                  (error) => {
+                    if (error) {
+                      console.log(error);
+                    } else {
+                      console.log("Non Recursive: Directories Deleted!");
+                    }
+                  }
+                );
+                res.send(err.message);
+                // console.log(err.message);
+              } else {
+                console.log(req.body.id);
+
+                console.log(results);
+                fs.rmdir(
+                  `${req.body.id}`,
+                  {
+                    recursive: true,
+                  },
+                  (error) => {
+                    if (error) {
+                      console.log(error);
+                    } else {
+                      console.log("Non Recursive: Directories Deleted!");
+                    }
+                  }
+                );
+                res.send(results);
+              }
             }
-            console.log(results);
-            res.send(results);
-          });
+          );
         } else if (req.body.name == "javascript") {
           //   eval(req.body.code).then((val) =>
           //     console.log("asynchronous logging has val:", val)
           //   );
-    
-          await fs.writeFile(`${req.body.id}/run.js`, req.body.code, function (err) {
+
+          fs.writeFile(`${req.body.id}/run.js`, req.body.code, function (err) {
             if (err) throw err;
             console.log("Saved!");
             exec(`node ${req.body.id}/run.js`, function (err, stdout, stderr) {
@@ -50,51 +83,109 @@ app.post("/", async (req, res) => {
                 // console.error(`exec error 0000000: ${error}`);
                 // console.error(error.name);
                 // console.error(stderr);
+                fs.rmdir(
+                  `${req.body.id}`,
+                  {
+                    recursive: true,
+                  },
+                  (error) => {
+                    if (error) {
+                      console.log(error);
+                    } else {
+                      console.log("Non Recursive: Directories Deleted!");
+                    }
+                  }
+                );
                 res.send(stderr);
+              } else {
+                fs.rmdir(
+                  `${req.body.id}`,
+                  {
+                    recursive: true,
+                  },
+                  (error) => {
+                    if (error) {
+                      console.log(error);
+                    } else {
+                      console.log("Non Recursive: Directories Deleted!");
+                    }
+                  }
+                );
+                res.send(stdout);
+
+                console.log(stdout);
               }
-              res.send(stdout);
-    
-              console.log(stdout);
             });
           });
-          //   if (shell.exec("run.js").code !== 0) {
-          //     shell.echo("Error: Git commit failed");
-          //     shell.exit(1);
-          //   }
-          //   await exec("node run.js", function (err, stdout, stderr) {
-          //     // handle err, stdout, stderr
-          //     if (error) {
-          //       // console.error(`exec error 0000000: ${error}`);
-          //       console.error(error.name);
-          //       console.error(stderr);
-          //       res.send(stderr);
-          //     }
-          //     res.send(stdout);
-    
-          //     console.log(stdout);
-          //   });
         } else if (req.body.name == "c") {
           fs.writeFile(`${req.body.id}/run.c`, req.body.code, function (err) {
             if (err) throw err;
             console.log("Saved!");
           });
-          exec(`gcc ${req.body.id}/run.c -o run`, (error, stdout, stderr) => {
+          exec(`cd ${req.body.id} && gcc run.c -o run`, (error, stdout, stderr) => {
             if (error) {
+              console.log("error")
               // console.error(`exec error 0000000: ${error}`);
               // console.error(error.name);
               // console.error(stderr);
+              fs.rmdir(
+                `${req.body.id}`,
+                {
+                  recursive: true,
+                },
+                (error) => {
+                  if (error) {
+                    console.log(error);
+                  } else {
+                    console.log("Non Recursive: Directories Deleted!");
+                  }
+                }
+              );
               res.send(stderr);
             } else {
-              exec(`${req.body.id}/run`, (error, stdout, stderr) => {
+              console.log("1")
+
+              exec(`cd ${req.body.id} && ./run`, (error, stdout, stderr) => {
                 console.log(`stdout: ${stdout}`);
                 console.log(`stderr: ${stderr}`);
                 if (error) {
+              console.log("2")
+
                   console.error(`exec error: ${error}`);
+                  fs.rmdir(
+                    `${req.body.id}`,
+                    {
+                      recursive: true,
+                    },
+                    (error) => {
+                      if (error) {
+                        console.log(error);
+                      } else {
+                        console.log("Non Recursive: Directories Deleted!");
+                      }
+                    }
+                  );
                   res.send(error);
-    
+
                   return;
+                } else {
+              console.log("2")
+
+                  fs.rmdir(
+                    `${req.body.id}`,
+                    {
+                      recursive: true,
+                    },
+                    (error) => {
+                      if (error) {
+                        console.log(error);
+                      } else {
+                        console.log("Non Recursive: Directories Deleted!");
+                      }
+                    }
+                  );
+                  res.send(stdout);
                 }
-                res.send(stdout);
               });
             }
           });
@@ -103,37 +194,97 @@ app.post("/", async (req, res) => {
             if (err) throw err;
             console.log("Saved!");
           });
-    
-          exec(`g++ ${req.body.id}/run.cpp -o teste`, (error, stdout, stderr) => {
-            if (error) {
-              // console.error(`exec error 0000000: ${error}`);
-              // console.error(error.name);
-              // console.error(stderr);
-              res.send(stderr);
-            } else {
-              exec(`${req.body.id}/teste`, (error, stdout, stderr) => {
-                console.log(`stdout: ${stdout}`);
-                console.log(`stderr: ${stderr}`);
-                if (error) {
-                  console.error(`exec error: ${error}`);
-                  res.send(error);
-    
-                  return;
-                }
-                res.send(stdout);
-              });
+
+          exec(
+            `cd ${req.body.id} && g++ run.cpp -o teste`,
+            (error, stdout, stderr) => {
+              if (error) {
+                // console.error(`exec error 0000000: ${error}`);
+                // console.error(error.name);
+                // console.error(stderr);
+                fs.rmdir(
+                  `${req.body.id}`,
+                  {
+                    recursive: true,
+                  },
+                  (error) => {
+                    if (error) {
+                      console.log(error);
+                    } else {
+                      console.log("Non Recursive: Directories Deleted!");
+                    }
+                  }
+                );
+                res.send(stderr);
+              } else {
+                exec(`cd ${req.body.id} && ./teste`, (error, stdout, stderr) => {
+                  console.log(`stdout: ${stdout}`);
+                  console.log(`stderr: ${stderr}`);
+                  if (error) {
+                    console.error(`exec error: ${error}`);
+                    fs.rmdir(
+                      `${req.body.id}`,
+                      {
+                        recursive: true,
+                      },
+                      (error) => {
+                        if (error) {
+                          console.log(error);
+                        } else {
+                          console.log("Non Recursive: Directories Deleted!");
+                        }
+                      }
+                    );
+                    res.send(error);
+
+                    return;
+                  } else {
+                    fs.rmdir(
+                      `${req.body.id}`,
+                      {
+                        recursive: true,
+                      },
+                      (error) => {
+                        if (error) {
+                          console.log(error);
+                        } else {
+                          console.log("Non Recursive: Directories Deleted!");
+                        }
+                      }
+                    );
+                    res.send(stdout);
+                  }
+                });
+              }
             }
-          });
+          );
         } else if (req.body.name == "java") {
-          fs.writeFile(`${req.body.id}/run.java`, req.body.code, function (err) {
-            if (err) throw err;
-            console.log("Saved!");
-          });
+          fs.writeFile(
+            `${req.body.id}/run.java`,
+            req.body.code,
+            function (err) {
+              if (err) throw err;
+              console.log("Saved!");
+            }
+          );
           exec(`javac ${req.body.id}/run.java`, (error, stdout, stderr) => {
             if (error) {
               console.error(`exec error 0000000: ${error}`);
               console.error(error.name);
               console.error(stderr);
+              fs.rmdir(
+                `${req.body.id}`,
+                {
+                  recursive: true,
+                },
+                (error) => {
+                  if (error) {
+                    console.log(error);
+                  } else {
+                    console.log("Non Recursive: Directories Deleted!");
+                  }
+                }
+              );
               res.send(stderr);
             } else {
               exec(`cd ${req.body.id} && java run`, (error, stdout, stderr) => {
@@ -141,36 +292,47 @@ app.post("/", async (req, res) => {
                 console.log(`stderr: ${stderr}`);
                 if (error) {
                   console.error(`exec error: ${error}`);
+                  fs.rmdir(
+                    `${req.body.id}`,
+                    {
+                      recursive: true,
+                    },
+                    (error) => {
+                      if (error) {
+                        console.log(error);
+                      } else {
+                        console.log("Non Recursive: Directories Deleted!");
+                      }
+                    }
+                  );
                   res.send(error);
-    
+
                   return;
+                } else {
+                  fs.rmdir(
+                    `${req.body.id}`,
+                    {
+                      recursive: true,
+                    },
+                    (error) => {
+                      if (error) {
+                        console.log(error);
+                      } else {
+                        console.log("Non Recursive: Directories Deleted!");
+                      }
+                    }
+                  );
+                  res.send(stdout);
                 }
-                res.send(stdout);
               });
             }
           });
         }
-        fs.rmdir(
-          `${req.body.id}`,
-          {
-            recursive: true,
-          },
-          (error) => {
-            if (error) {
-              console.log(error);
-            } else {
-              console.log("Non Recursive: Directories Deleted!");
-            }
-          }
-        );
       }
     });
-
-
-    
-  } catch (error) { }
+  } catch (error) {}
 });
 
-app.listen(3340, () => {
+app.listen(3341, () => {
   console.log("started");
 });
