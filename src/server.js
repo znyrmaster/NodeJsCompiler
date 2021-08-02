2907fdedb1a63e5000ebac90c9f472adb1507c6d
znyrsam <znyrsam@gmail.com>
2021-08-02 05:05:56 +0000

    matplotlib added

--- src/server.js
+++ src/server.js
@@ -7,6 +7,13 @@ const { exec } = require("child_process");
 var shell = require("shelljs");
 const app = express();
 
+
+
+app.use(express.static('public'));
+
+//Serves all the request which includes /images in the url from Images folder
+app.use('/images', express.static(__dirname + '/Images'));
+
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -31,52 +38,183 @@ app.post("/", async (req, res) => {
         console.log(err);
       } else {
         if (req.body.name == "python") {
-          fs.writeFile(`${req.body.id}/run.py`, req.body.code, function (err) {
+
+          console.log(req.body.code)
+
+          let abc = req.body.code
+
+          // new code
+          let indices;
+          var count;
+          var pcount;
+
+          if (abc.includes("matplotlib")) {
+            function countOccurences(string, word) {
+              return string.split(word).length - 1;
+            }
+            count = countOccurences(abc, ".show()"); // 2
+
+
+
+            for (let i = 0; i < count; i++) {
+              abc = abc.replace(".show()", `.savefig('public/Images/${req.body.id}-${i}.png')`)
+              // abc = abc.replace(".show()", `.savefig('public/Images/abc-${i}.png')`)
+            }
+            console.log(count)
+            console.log(abc)
+
+
+          } else {
+            console.log("nothing")
+          }
+
+          // if (abc.includes("plotly")) {
+          //   function countOccurences(string, word) {
+          //     return string.split(word).length - 1;
+          //   }
+          //   pcount = countOccurences(abc, ".show()"); // 2
+
+
+
+          //   for (let i = 0; i < pcount; i++) {
+          //     abc = abc.replace(".show()", `.to_dict()\nprint(fig.to_dict())\n`)
+          //     // abc = abc.replace(".show()", `.savefig('public/Images/abc-${i}.png')`)
+          //   }
+          //   console.log(pcount)
+          //   console.log(abc)
+
+
+          // } else {
+          //   console.log("nothing")
+          // }
+
+
+
+
+
+
+
+
+          // new code
+
+          console.log("abc", abc)
+          fs.writeFile(`${req.body.id}/run.py`, abc, function (err) {
             if (err) throw err;
             console.log("Saved!");
           });
-          PythonShell.run(
-            `${req.body.id}/run.py`,
-            null,
-            function (err, results) {
-              if (err) {
-                console.log(req.body.id);
-                fs.rmdir(
-                  `${req.body.id}`,
-                  {
-                    recursive: true,
-                  },
-                  (error) => {
-                    if (error) {
-                      console.log(error);
-                    } else {
-                      console.log("Non Recursive: Directories Deleted!");
-                    }
+          exec(`python ${req.body.id}/run.py`, function (error, stdout, stderr) {
+            // handle err, stdout, stderr
+            console.log("opp", error, stdout, stderr)
+            if (error) {
+              console.error(`exec error 0000000: ${error}`);
+              console.error(error.name);
+              console.error(stderr);
+              fs.rmdir(
+                `${req.body.id}`,
+                {
+                  recursive: true,
+                },
+                (error) => {
+                  if (error) {
+                    console.log(error);
+                  } else {
+                    console.log("Non Recursive: Directories Deleted!");
                   }
-                );
-                res.send(err.message);
-                // console.log(err.message);
-              } else {
-                console.log(req.body.id);
+                }
+              );
+              res.send(stderr);
+            } else {
+              fs.rmdir(
+                `${req.body.id}`,
+                {
+                  recursive: true,
+                },
+                (error) => {
+                  if (error) {
+                    console.log(error);
+                  } else {
+                    console.log("Non Recursive: Directories Deleted!");
+                  }
+                }
+              );
+              console.log("else", stdout)
 
-                console.log(results);
-                fs.rmdir(
-                  `${req.body.id}`,
-                  {
-                    recursive: true,
-                  },
-                  (error) => {
-                    if (error) {
-                      console.log(error);
-                    } else {
-                      console.log("Non Recursive: Directories Deleted!");
-                    }
-                  }
-                );
-                res.send(results);
+              if (count > 0) {
+                console.log("matplotlib")
+                // res.sendFile("NodeJsCompiler/pavitest2@gmail.com/0.png");
+                // let abc = {"type":"image",img:[]}
+                let img = []
+                for (let i = 0; i < count; i++) {
+
+                  img.push(`Images/${req.body.id}-${i}.png`)
+                }
+                res.send(img)
+
               }
+              // else if(pcount > 0) {
+              //   console.log("plotly")
+
+              //   let out = {
+              //     type:'plotly',
+              //     data :stdout
+              //   }
+              //   res.json(stdout);
+
+              // }
+              else {
+                console.log("nothing")
+
+                res.send(stdout);
+
+              }
+
+              console.log(stdout);
+              console.log(typeof stdout);
             }
-          );
+          });
+          // PythonShell.run(
+          //   `${req.body.id}/run.py`,
+          //   null,
+          //   function (err, results) {
+          //     if (err) {
+          //       console.log(req.body.id);
+          //       // fs.rmdir(
+          //       //   `${req.body.id}`,
+          //       //   {
+          //       //     recursive: true,
+          //       //   },
+          //       //   (error) => {
+          //       //     if (error) {
+          //       //       console.log(error);
+          //       //     } else {
+          //       //       console.log("Non Recursive: Directories Deleted!");
+          //       //     }
+          //       //   }
+          //       // );
+          //       res.send(err.message);
+          //       // console.log(err.message);
+          //     } else {
+          //       console.log(req.body.id);
+
+          //       console.log(results);
+          //       console.log("working");
+          //       // fs.rmdir(
+          //       //   `${req.body.id}`,
+          //       //   {
+          //       //     recursive: true,
+          //       //   },
+          //       //   (error) => {
+          //       //     if (error) {
+          //       //       console.log(error);
+          //       //     } else {
+          //       //       console.log("Non Recursive: Directories Deleted!");
+          //       //     }
+          //       //   }
+          //       // );
+          //       res.send(results);
+          //     }
+          //   }
+          // );
         } else if (req.body.name == "javascript") {
           //   eval(req.body.code).then((val) =>
           //     console.log("asynchronous logging has val:", val)
@@ -344,9 +482,14 @@ app.post("/", async (req, res) => {
         }
       }
     });
-  } catch (error) {}
+  } catch (error) { }
 });
 
 app.listen(3334, () => {
   console.log("started");
 });
+
+
+// app.listen(3004, () => {
+//   console.log("started");
+// });
